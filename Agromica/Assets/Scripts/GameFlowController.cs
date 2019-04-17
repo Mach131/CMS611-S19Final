9d1c72cabee69369d457852288d750002c4a7570
Temi Taylor <temit@mit.edu>
2019-04-17 14:55:18 -0400

    starting to implement game flow controller

--- Agromica/Assets/Scripts/GameFlowController.cs
+++ Agromica/Assets/Scripts/GameFlowController.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls the flow of the game, particularly initialization and the changing of state between turns.
+/// </summary>
 public class GameFlowController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Scenario Data")]
+    public int numberOfRounds;
+    public List<Crop> availableCrops;
+    public List<Quota> quotas;
+    [Header("Scene References")]
+    public Player playerObject;
+    //TODO: market
+
+    private int currentTurn;
+
+    /// <summary>
+    /// Represents a crop, containing data that the market needs to be initialized
+    /// </summary>
+    [System.Serializable]
+    public class Crop
     {
-        
+        public string cropName;
+        //TODO: stuff that the market needs to know (reactivity, starting prices)
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Represents a quota, containing information about when it is due and what is required.
+    /// </summary>
+    [System.Serializable]
+    public class Quota
     {
-        
+        //when the quota is due
+        public int turnNumber;
+        //maps the index of the crop (in availableCrops) to how many are needed
+        public Dictionary<int, int> cropIndexToRequiredAmount;
+    }
+
+
+    /// <summary>
+    /// Initialize the scenario
+    /// </summary>
+    private void Start()
+    {
+        //TODO: send crop info to market
+        currentTurn = 0;
     }
 }
