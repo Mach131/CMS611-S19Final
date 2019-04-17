743a09b39302ddceb9daab45535543ca1894ea98
gfaline <gwenf@mit.edu>
2019-04-17 16:37:23 -0400

    Not complete, but you can plant a thing in a plot once per turn and update the turn

--- Agromica/Assets/Scripts/Plot.cs
+++ Agromica/Assets/Scripts/Plot.cs
@@ -2,20 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Represents a plot of land on a farm, where a seed can be planted.
-/// </summary>
 public class Plot : MonoBehaviour
 {
+
+    public bool isRed = false;
+    public bool isBlue = false;
+    public Canvas plotMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Harvest()
     {
-        
+        // For now, return 1 for red and 2 for blue
+        // Should be an onClick function
+        // Needs to be related to self
+        if (isRed)
+        {
+            isRed = false;
+            TurnControl.PLOTS.Remove(this.gameObject);
+            return 1;
+        }
+        if (isBlue)
+        {
+            isBlue = false;
+            TurnControl.PLOTS.Remove(this.gameObject);
+            return 2;
+        }
+        return 0;
+    }
+
+    public void Plant(int type)
+    {
+        // Once there is a plant type, this will pass that instead
+        if (type == 1)
+        {
+            TurnControl.PLOTS.Add(this.gameObject);
+            this.plotMenu.gameObject.SetActive(false);
+            isRed = true;
+        }
+        else if (type == 2)
+        {
+            TurnControl.PLOTS.Add(this.gameObject);
+            this.plotMenu.gameObject.SetActive(false);
+            isBlue = true;
+        }
+    }
+
+    public void PlotMenu(Canvas plotMenu)
+    {
+        Debug.Log("I have been clicked");
+        if (!(isRed || isBlue)) 
+        {
+        this.plotMenu = plotMenu;
+        this.plotMenu.gameObject.SetActive(true); 
+        }
     }
 }
---
+++ Agromica/Assets/Scripts/TurnControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnControl : MonoBehaviour
+{
+    public static List<GameObject> PLOTS;
+    public static int TURN = 0;
+    public static int BLUE_OWNED = 0;
+    public static int RED_OWNED = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PLOTS = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateTurn()
+    {
+        // Need to update turn counter
+        // Need to update market
+        // Need to grow crops
+        TurnControl.TURN += 1;
+        Debug.Log("Next turn");
+
+        while (TurnControl.PLOTS.Count > 0)
+        {
+            Plot plot = TurnControl.PLOTS[0].GetComponent<Plot>();
+            int item = plot.Harvest();
+            Debug.Log(item);
+            TurnControl.PLOTS.Remove(plot.gameObject);
+        }
+    }
+}
