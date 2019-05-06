799692ee6ab58b2c15b63afe8d91b3acfa507627
Temi Taylor <temit@mit.edu>
2019-05-06 02:16:43 -0400

    Setting up scene transition button

---
+++ Agromica/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Sets up a transition to another scene.
+/// </summary>
+public class SceneTransition : MonoBehaviour
+{
+    public string targetSceneName;
+
+    public void goToTargetScene()
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
