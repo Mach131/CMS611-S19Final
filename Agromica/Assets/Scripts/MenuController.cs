901a9f21e0048863c7ff10731e02e75dcf6afecc
Temi Taylor <temit@mit.edu>
2019-05-06 03:23:41 -0400

    Adding instructions interface

---
+++ Agromica/Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Facilitates the showing and hiding of a menu like the instructions panel
+/// </summary>
+public class MenuController : MonoBehaviour
+{
+    public GameObject menu;
+    public bool visibleAtStart;
+
+    private void Start()
+    {
+        menu.SetActive(visibleAtStart);
+    }
+
+    /// <summary>
+    /// Updates the visibility of the menu
+    /// </summary>
+    /// <param name="newStatus">true if the menu should be visible, false otherwise</param>
+    public void SetMenuStatus(bool newStatus)
+    {
+        menu.SetActive(newStatus);
+    }
+}
