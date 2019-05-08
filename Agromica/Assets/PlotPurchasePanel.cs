4a808a31ed1ae501bad94476fc0e529a608d76f3
Cameron Ordone <ceordone@gmail.com>
2019-05-08 16:04:14 -0400

    UI fixes
    plot purchasing panel shows plot price
    the plots are white
    bank acknowledges the existence of interest

---
+++ Agromica/Assets/PlotPurchasePanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlotPurchasePanel : MonoBehaviour
+{
+    private TextMeshProUGUI message;
+    
+    void Awake()
+    {
+        message = transform.Find("Text Blurb").GetComponent<TextMeshProUGUI>();
+    }
+
+    void OnEnable()
+    {
+        message.text = string.Format("Purchase this plot?\n{0} rupees", Plot.plotPrice);
+    }
+
+}
---
+++ Agromica/Assets/Scripts/UI/PopupMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupMessage : MonoBehaviour
+{
+    void Awake()
+    {
+        this.gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+    }
+
+    void OnDisable()
+    {
+        Destroy(this.gameObject, 1.0f);
+    }
+}
