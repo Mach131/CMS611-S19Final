248d74b60cb6e87ef9fb1490316f9b4245b5aa03
Temi Taylor <temit@mit.edu>
2019-05-13 03:03:19 -0400

    Implemented button sound effects

---
+++ Agromica/Assets/Scripts/AttachButtonSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Provides an easier way to set up buttons to play the appropriate sound when clicked.
+/// </summary>
+public class AttachButtonSound : MonoBehaviour
+{
+    private void Start()
+    {
+        SoundEffectController sfxController = FindObjectOfType<SoundEffectController>();
+        GetComponent<Button>().onClick.AddListener(() => sfxController.playButtonSound());
+    }
+}
---
+++ Agromica/Assets/Scripts/SoundEffectController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the playing of sound effects for things like buttons being pressed.
+/// </summary>
+public class SoundEffectController : MonoBehaviour
+{
+    public AudioClip buttonSoundEffect;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    /// <summary>
+    /// The sound for pressing a button.
+    /// </summary>
+    public void playButtonSound()
+    {
+        audioSource.PlayOneShot(buttonSoundEffect);
+    }
+}
